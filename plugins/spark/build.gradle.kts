914567a5ded2d1b5473160a704efdd0ccb9a6dc0
Yun Zou <yun.zou@snowflake.com>
2025-02-27 19:31:45 -0800

    run spotless

--- plugins/spark/build.gradle.kts
+++ plugins/spark/build.gradle.kts
@@ -17,32 +17,80 @@
  * under the License.
  */
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 plugins {
-    alias(libs.plugins.jandex)
+  id("polaris-client")
+  // id("com.gradleup.shadow")
+  // alias(libs.plugins.jandex)
 }
 
 val sparkMajorVersion = "3.5"
 val scalaVersion = "2.12"
 
 dependencies {
-    implementation(project(":polaris-core"))
-    implementation(project(":polaris-api-management-service"))
-    implementation(project(":polaris-api-iceberg-service"))
-    implementation(project(":polaris-service-common"))
+  implementation(project(":polaris-core"))
+  // implementation(project(":polaris-api-management-service"))
+  // implementation(project(":polaris-api-iceberg-service"))
 
-    // implementation("com.fasterxml.jackson.core:jackson-annotations")
-    // implementation("com.fasterxml.jackson.core:jackson-core")
-    // implementation("com.fasterxml.jackson.core:jackson-databind")
-    // spark dependencies
+  implementation(libs.guava)
+  implementation(platform(libs.iceberg.bom))
+  implementation("org.apache.iceberg:iceberg-api:1.8.0")
+  implementation("org.apache.iceberg:iceberg-core:1.8.0")
+  implementation("org.apache.iceberg:iceberg-spark-3.5_2.12:1.8.0")
+  implementation("org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.8.0")
+  implementation("org.apache.iceberg:iceberg-spark-extensions-3.5_2.12:1.8.0")
+
+  compileOnly("org.apache.spark:spark-sql_${scalaVersion}:${sparkMajorVersion}.2") {
+    // exclude log4j dependencies
+    exclude("org.apache.logging.log4j", "log4j-slf4j2-impl")
+    exclude("org.apache.logging.log4j", "log4j-api")
+    exclude("org.apache.logging.log4j", "log4j-1.2-api")
+    exclude("org.slf4j", "jul-to-slf4j")
+  }
+  // implementation("com.fasterxml.jackson.core:jackson-annotations")
+  // implementation("com.fasterxml.jackson.core:jackson-core")
+  // implementation("com.fasterxml.jackson.core:jackson-databind")
+  // spark dependencies
 }
 
-sourceSets {
-    main {
-        java.srcDirs("src/main/java")
-    }
-}
+sourceSets { main { java.srcDirs("src/main/java") } }
 
-tasks.register<Jar>("createPolarisSparkJar") {
-    archiveBaseName = "polaris-spark-runtime-${sparkMajorVersion}_${scalaVersion}"
-    from(sourceSets.main.get().output)
+tasks.register<ShadowJar>("createPolarisSparkJar") {
+  duplicatesStrategy = DuplicatesStrategy.INCLUDE
+  archiveBaseName = "polaris-spark-runtime-${sparkMajorVersion}_${scalaVersion}"
+  from(sourceSets.main.get().output)
+  from(
+    project.configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+  )
+  // Optional: Minimize the JAR (remove unused classes from dependencies)
+  minimize()
+
+  // Example of using afterEvaluate to modify task after evaluation
+  afterEvaluate {
+    // Safe to modify configuration here
+    println("ShadowJar task has been evaluated!")
+  }
+  // Include the specific dependencies you want in the JAR
+  // from(configurations.runtimeClasspath.get().filter {
+  // Include the iceberg spark jars
+  //    it.name.contains("iceberg-spark")
+  // }.map { if (it.isDirectory) it else zipTree(it) })
 }
--- plugins/spark/src/main/java/org/apache/polaris/spark/SparkCatalog.java
+++ plugins/spark/src/main/java/org/apache/polaris/spark/SparkCatalog.java
@@ -16,13 +16,233 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.polaris.spark;
 
-// import org.apache.spark.sql.connector.catalog.TableCatalog;
-// import org.apache.spark.sql.connector.catalog.SupportsNamespaces;
-// import org.apache.spark.sql.connector.catalog.ViewCatalog;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.CatalogProperties;
+import org.apache.iceberg.CatalogUtil;
+import org.apache.iceberg.catalog.Catalog;
+import org.apache.iceberg.catalog.Namespace;
+import org.apache.iceberg.exceptions.AlreadyExistsException;
+import org.apache.iceberg.hadoop.HadoopCatalog;
+import org.apache.iceberg.spark.SparkUtil;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.catalyst.analysis.NamespaceAlreadyExistsException;
+import org.apache.spark.sql.catalyst.analysis.NoSuchNamespaceException;
+import org.apache.spark.sql.catalyst.analysis.NoSuchTableException;
+import org.apache.spark.sql.catalyst.analysis.TableAlreadyExistsException;
+import org.apache.spark.sql.connector.catalog.*;
+import org.apache.spark.sql.connector.catalog.Table;
+import org.apache.spark.sql.connector.expressions.Transform;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.util.CaseInsensitiveStringMap;
 
-public class SparkCatalog {
+public class SparkCatalog implements TableCatalog, SupportsNamespaces {
+  private static final Set<String> DEFAULT_NS_KEYS = ImmutableSet.of(TableCatalog.PROP_OWNER);
+  private String catalogName = null;
+  private Catalog icebergCatalog = null;
+  private PolarisRESTCatalog polarisCatalog = null;
+  private String[] defaultNamespace = null;
+  private org.apache.iceberg.catalog.SupportsNamespaces asNamespaceCatalog = null;
+  private org.apache.iceberg.catalog.ViewCatalog asViewCatalog = null;
 
+  protected Catalog buildIcebergCatalog(String name, CaseInsensitiveStringMap options) {
+    Configuration conf = SparkUtil.hadoopConfCatalogOverrides(SparkSession.active(), name);
+    Map<String, String> optionsMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    optionsMap.putAll(options.asCaseSensitiveMap());
+    optionsMap.put(CatalogProperties.APP_ID, SparkSession.active().sparkContext().applicationId());
+    optionsMap.put(CatalogProperties.USER, SparkSession.active().sparkContext().sparkUser());
+    return CatalogUtil.buildIcebergCatalog(name, optionsMap, conf);
+  }
+
+  protected PolarisRESTCatalog buildPolarisCatalog(String name, CaseInsensitiveStringMap options) {
+    Configuration conf = SparkUtil.hadoopConfCatalogOverrides(SparkSession.active(), name);
+    Map<String, String> optionsMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+    optionsMap.putAll(options.asCaseSensitiveMap());
+    optionsMap.put(CatalogProperties.APP_ID, SparkSession.active().sparkContext().applicationId());
+    optionsMap.put(CatalogProperties.USER, SparkSession.active().sparkContext().sparkUser());
+
+    PolarisRESTCatalog catalog = new PolarisRESTCatalog();
+    catalog.setConf(conf);
+    catalog.initialize(name, optionsMap);
+
+    return catalog;
+  }
+
+  @Override
+  public String name() {
+    return catalogName;
+  }
+
+  @Override
+  public void initialize(String name, CaseInsensitiveStringMap options) {
+    this.catalogName = name;
+    this.icebergCatalog = buildIcebergCatalog(name, options);
+    this.polarisCatalog = buildPolarisCatalog(name, options);
+
+    this.asNamespaceCatalog = (org.apache.iceberg.catalog.SupportsNamespaces) this.icebergCatalog;
+    this.asViewCatalog = (org.apache.iceberg.catalog.ViewCatalog) this.icebergCatalog;
+  }
+
+  @Override
+  public Table loadTable(Identifier ident) throws NoSuchTableException {
+    throw new NoSuchTableException(ident);
+  }
+
+  @Override
+  public Table createTable(
+      Identifier ident, StructType schema, Transform[] transforms, Map<String, String> properties)
+      throws TableAlreadyExistsException {
+    throw new TableAlreadyExistsException(ident);
+  }
+
+  @Override
+  public Table alterTable(Identifier ident, TableChange... changes) throws NoSuchTableException {
+    throw new NoSuchTableException(ident);
+  }
+
+  @Override
+  public boolean dropTable(Identifier ident) {
+    return false;
+  }
+
+  @Override
+  public void renameTable(Identifier from, Identifier to)
+      throws NoSuchTableException, TableAlreadyExistsException {
+    throw new NoSuchTableException(from);
+  }
+
+  @Override
+  public Identifier[] listTables(String[] namespace) {
+    return polarisCatalog.listTables(Namespace.of(namespace)).stream()
+        .map(ident -> Identifier.of(ident.namespace().levels(), ident.name()))
+        .toArray(Identifier[]::new);
+  }
+
+  @Override
+  public String[] defaultNamespace() {
+    if (defaultNamespace != null) {
+      return defaultNamespace;
+    }
+
+    return new String[0];
+  }
+
+  @Override
+  public String[][] listNamespaces() {
+    if (asNamespaceCatalog != null) {
+      return asNamespaceCatalog.listNamespaces().stream()
+          .map(Namespace::levels)
+          .toArray(String[][]::new);
+    }
+
+    return new String[0][];
+  }
+
+  @Override
+  public String[][] listNamespaces(String[] namespace) throws NoSuchNamespaceException {
+    if (asNamespaceCatalog != null) {
+      try {
+        return asNamespaceCatalog.listNamespaces(Namespace.of(namespace)).stream()
+            .map(Namespace::levels)
+            .toArray(String[][]::new);
+      } catch (org.apache.iceberg.exceptions.NoSuchNamespaceException e) {
+        throw new NoSuchNamespaceException(namespace);
+      }
+    }
+
+    throw new NoSuchNamespaceException(namespace);
+  }
+
+  @Override
+  public Map<String, String> loadNamespaceMetadata(String[] namespace)
+      throws NoSuchNamespaceException {
+    if (asNamespaceCatalog != null) {
+      try {
+        return asNamespaceCatalog.loadNamespaceMetadata(Namespace.of(namespace));
+      } catch (org.apache.iceberg.exceptions.NoSuchNamespaceException e) {
+        throw new NoSuchNamespaceException(namespace);
+      }
+    }
+
+    throw new NoSuchNamespaceException(namespace);
+  }
+
+  @Override
+  public void createNamespace(String[] namespace, Map<String, String> metadata)
+      throws NamespaceAlreadyExistsException {
+    if (asNamespaceCatalog != null) {
+      try {
+        if (asNamespaceCatalog instanceof HadoopCatalog
+            && DEFAULT_NS_KEYS.equals(metadata.keySet())) {
+          // Hadoop catalog will reject metadata properties, but Spark automatically adds "owner".
+          // If only the automatic properties are present, replace metadata with an empty map.
+          asNamespaceCatalog.createNamespace(Namespace.of(namespace), ImmutableMap.of());
+        } else {
+          asNamespaceCatalog.createNamespace(Namespace.of(namespace), metadata);
+        }
+      } catch (AlreadyExistsException e) {
+        throw new NamespaceAlreadyExistsException(namespace);
+      }
+    } else {
+      throw new UnsupportedOperationException(
+          "Namespaces are not supported by catalog: " + catalogName);
+    }
+  }
+
+  @Override
+  public void alterNamespace(String[] namespace, NamespaceChange... changes)
+      throws NoSuchNamespaceException {
+    if (asNamespaceCatalog != null) {
+      Map<String, String> updates = Maps.newHashMap();
+      Set<String> removals = Sets.newHashSet();
+      for (NamespaceChange change : changes) {
+        if (change instanceof NamespaceChange.SetProperty) {
+          NamespaceChange.SetProperty set = (NamespaceChange.SetProperty) change;
+          updates.put(set.property(), set.value());
+        } else if (change instanceof NamespaceChange.RemoveProperty) {
+          removals.add(((NamespaceChange.RemoveProperty) change).property());
+        } else {
+          throw new UnsupportedOperationException(
+              "Cannot apply unknown namespace change: " + change);
+        }
+      }
+
+      try {
+        if (!updates.isEmpty()) {
+          asNamespaceCatalog.setProperties(Namespace.of(namespace), updates);
+        }
+
+        if (!removals.isEmpty()) {
+          asNamespaceCatalog.removeProperties(Namespace.of(namespace), removals);
+        }
+
+      } catch (org.apache.iceberg.exceptions.NoSuchNamespaceException e) {
+        throw new NoSuchNamespaceException(namespace);
+      }
+    } else {
+      throw new NoSuchNamespaceException(namespace);
+    }
+  }
+
+  @Override
+  public boolean dropNamespace(String[] namespace, boolean cascade)
+      throws NoSuchNamespaceException {
+    if (asNamespaceCatalog != null) {
+      try {
+        return asNamespaceCatalog.dropNamespace(Namespace.of(namespace));
+      } catch (org.apache.iceberg.exceptions.NoSuchNamespaceException e) {
+        throw new NoSuchNamespaceException(namespace);
+      }
+    }
+
+    return false;
+  }
 }
