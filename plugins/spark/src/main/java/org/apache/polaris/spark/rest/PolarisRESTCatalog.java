ecb57b39b5ea9fc0d2aec3ea95770d396b804aac
Yun Zou <yun.zou@snowflake.com>
2025-03-28 20:24:45 -0700

    add change

---
+++ plugins/spark/src/main/java/org/apache/polaris/spark/rest/PolarisRESTCatalog.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.polaris.spark.rest;
+
+import org.apache.iceberg.CatalogProperties;
+import org.apache.iceberg.catalog.SessionCatalog;
+import org.apache.iceberg.rest.HTTPClient;
+import org.apache.iceberg.rest.RESTCatalog;
+import org.apache.iceberg.rest.RESTClient;
+import org.apache.iceberg.rest.auth.AuthManager;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class PolarisRESTCatalog extends RESTCatalog {
+    public PolarisRESTCatalog() {
+      this(
+          SessionCatalog.SessionContext.createEmpty(),
+          config -> HTTPClient.builder(config).uri(config.get(CatalogProperties.URI)).build());
+    }
+
+    public PolarisRESTCatalog(
+        SessionCatalog.SessionContext context,
+        Function<Map<String, String>, RESTClient> clientBuilder,
+        BiFunction<String, Map<String, String>, AuthManager> authManagerBuilder) {
+      super(context, clientBuilder, authManagerBuilder);
+    }
+}
